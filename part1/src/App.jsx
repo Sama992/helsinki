09b5acdd962deeeedddfba8d084abd929d09efc3
Sama992 <sushankunwar99@gmail.com>
2023-12-07 10:30:38 +0545

    part 1

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,34 +1,59 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+const Header = (props) => {
+  return (
+    <div>
+      <h1>{props.course}</h1>
 
-function App() {
-  const [count, setCount] = useState(0)
+    </div>
+  )
+
+}
+
+const Part = (props) => {
+  return (
+    <div>
+      <p>{props.part} {props.number}</p>
+    </div>
+  )
+
+}
+
+const Content = (props) => {
+  return (
+    <div>
+      <Part part = {props.part1} number = {props.number1}/>
+      <Part part = {props.part2} number = {props.number2}/>
+      <Part part = {props.part3} number = {props.number3}/>
+      
+      
+    </div>
+  )
+
+}
+const Total = (props) => {
+  return (
+    <div>
+      <p>{props.sum}</p>
+    </div>
+  )
+
+}
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <Header course = {course} />
+      <Content part1 = {part1} number1 = {exercises1} part2 = {part2} number2 = {exercises2}  part3 = {part3} number3 = {exercises3}/>
+      
+      <Total sum = {exercises1+exercises2+exercises3} />
+    </div>
   )
 }
 
