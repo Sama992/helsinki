1f43ebbddd3f1ed26a531a28ebfa8ab58bc96560
Sama992 <sushankunwar99@gmail.com>
2023-12-09 19:31:14 +0545

    unicafe complete

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,22 +1,90 @@
-const Hello = (props) => {
-  return (
-    <div>
-      <p>
-        Hello {props.name}, you are {props.age} years old
-      </p>
-    </div>
+import { useState } from 'react'
+
+
+const StatisticLine = (props) => {
+
+  return(
+    
+      
+          
+            <tr >
+              <td>{props.text}</td>
+              
+              <td>{props.value}</td>
+              </tr>
+
+      
+      
+      
+
+    
   )
 }
 
-const App = () => {
-  const name = 'Peter'
-  const age = 10
+const Statistics = (props) => {
+  const all = props.good + props.bad + props.neutral
+  const average = (props.good - props.bad)/ all
+  const positive = (props.good / all) * 100 + '%'
 
+if (all == 0) {
+  return(
+    <p>No feedback given</p>
+  )
+}
+else {
+  return(
+    <div>
+      <table>
+        <tbody>
+      <StatisticLine text = 'good' value = {props.good} />
+      <StatisticLine text = 'neutral' value = {props.neutral} />
+      <StatisticLine text = 'bad' value = {props.bad} />
+      <StatisticLine text = 'all' value = {all} />
+      <StatisticLine text = 'average' value = {average} />
+      <StatisticLine text = 'positive' value = {positive} />
+      </tbody>
+      </table>
+      </div>
+   
+  )
+  }
+
+
+}
+
+const Button = (props) => {
+
+
+  return(
+    
+      <button onClick={() => props.feedback(props.value+1)}>{props.text}</button>
+    
+  )
+
+}
+
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+  
+  
+
+ 
+  
+  
   return (
     <div>
-      <h1>Greetings</h1>
-      <Hello name="Maya" age={26 + 10} />
-      <Hello name={name} age={age} />
+      <h1>give feedback</h1>
+      <Button text = 'good' feedback = {setGood} value = {good} />
+      <Button text = 'neutral' feedback = {setNeutral} value = {neutral} />
+      <Button text = 'bad' feedback = {setBad} value = {bad} />
+      <h1>statistics</h1>
+      <Statistics good = {good} neutral = {neutral} bad = {bad} />
+      
+     
     </div>
   )
 }
